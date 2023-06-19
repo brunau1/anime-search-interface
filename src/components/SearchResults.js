764f2c89fc43c625039c58f8f402b0f95b3f5f03
Bruno Santos <bruno.santos@u4crypto.com>
2023-06-19 00:48:24 -0300

    feat: some treatments for improve the interface appearance

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,11 +1,16 @@
 import React from "react";
+import noCover from "../images/no-cover.png";
 
 function SearchResults({ results }) {
   return (
     <div>
       {results.map((result, index) => (
         <div className="list-item" key={index}>
-          <img className="anime-img" src={result.imageUrl} alt={result.title} />
+          <img
+            className="anime-img"
+            src={result.imageUrl === "no_cover" ? noCover : result.imageUrl}
+            alt={result.title}
+          />
           <div>
             <h3>{result.title}</h3>
             <p>{result.synopsis}</p>
